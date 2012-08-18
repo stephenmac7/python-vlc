3eeb6cddddae39637c7a19796aa4bab51a1170f9
Stephen McIntosh <stephenmac7@gmail.com>
2012-08-18 17:48:31 -0400

    Changed package name to vlclib

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='vlc',
+setup(name='python-vlc',
       version='0.1',
       description='A library to control or get information about VLC using Telnet',
       author='stephenmac7',
       author_email='moc liamg 7camnehpets, backwards',
       url='https://github.com/stephenmac7/python-vlc',
-      packages=['vlc']
+      packages=['vlclib']
      )

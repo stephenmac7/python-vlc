87ef00d119de1b68aa717fb9d455fad7ffc8baa8
stephenmac7 <stephenmac7@gmail.com>
2013-05-24 19:29:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='python-vlc',
       version='0.1',
       description='A library to control or get information about VLC using Telnet',
       author='stephenmac7',
-      author_email='moc liamg 7camnehpets, backwards',
+      author_email='stephenmac7@gmail.com',
       url='https://github.com/stephenmac7/python-vlc',
       packages=['vlclib']
      )
